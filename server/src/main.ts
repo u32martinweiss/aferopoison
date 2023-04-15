c8ff4ef82e69152d2813addcbd2bdcb09ac8d8e7
Martin Weiss <u32martinweiss@proton.me>
2023-04-15 13:11:17 +0200

    BE: Adding and setting up pg

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { Pool } from 'pg';
 
-import { DEFAULT_EXPRESS_PORT } from './utils/constants';
+import {
+  DEFAULT_EXPRESS_PORT,
+  DEFAULT_POSTGRES_PORT,
+  DEFAULT_POSTGRES_HOST,
+} from './utils/constants';
 
 // Setup
 const app = express();
@@ -12,6 +17,27 @@ dotenv.config();
 
 // Ports
 const expressPort = Number(process.env.EXPRESS_PORT) || DEFAULT_EXPRESS_PORT;
+const postgresPort = Number(process.env.POSTGRES_PORT) || DEFAULT_POSTGRES_PORT;
+
+// PostgreSQL Pool
+export const pool = new Pool({
+  host: process.env.DB_HOST || DEFAULT_POSTGRES_HOST,
+  user: process.env.DB_USER || '',
+  password: process.env.DB_PASS || '',
+  database: process.env.DB_NAME || '',
+  port: postgresPort,
+});
+
+// Validating PostgreSQL Connection
+pool
+  .connect()
+  .then(() => {
+    console.log(`[STATUS] PostgreSQL working on port ${postgresPort}...`);
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log(`[ERROR] PostgreSQL is not running!`);
+  });
 
 // Express Server
 app.listen(expressPort, () => {
