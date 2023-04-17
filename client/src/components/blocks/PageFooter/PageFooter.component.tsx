89a5fd9fba2a032369c23fd5764d28660fc316b3
Martin Weiss <u32martinweiss@proton.me>
2023-04-17 21:23:35 +0200

    FE: Creating the Page Footer block

---
+++ client/src/components/blocks/PageFooter/PageFooter.component.tsx
@@ -0,0 +1,8 @@
+const PageFooter = (): JSX.Element => (
+  <footer>
+    <hr />
+    <p>Aferpoison &copy; 2023</p>
+  </footer>
+);
+
+export default PageFooter;
---
+++ client/src/components/blocks/PageFooter/index.ts
@@ -0,0 +1,3 @@
+import PageFooter from './PageFooter.component';
+
+export default PageFooter;
