2534f4c0e75c3e2e848f43acabc480f09678b3d1
Martin Weiss <u32martinweiss@proton.me>
2023-04-17 21:22:01 +0200

    FE: Creating the Page Navigation block

--- client/src/layouts/DefaultLayout.tsx
+++ client/src/layouts/DefaultLayout.tsx
@@ -1,7 +1,14 @@
+import PageNavigation from '../components/blocks/PageNavigation';
+
 interface IProps {
   children: React.ReactNode;
 }
 
-const DefaultLayout = ({ children }: IProps) => <>{children}</>;
+const DefaultLayout = ({ children }: IProps) => (
+  <>
+    <PageNavigation />
+    {children}
+  </>
+);
 
 export default DefaultLayout;
