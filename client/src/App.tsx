280cf16a671339929abce939f3232830575b7ce6
Martin Weiss <u32martinweiss@proton.me>
2023-04-18 10:48:23 +0200

    FE: Scalable font size

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 
 import './styles/reset.scss';
+import './styles/main.scss';
 
 const App = (): JSX.Element => (
   <Routes>
