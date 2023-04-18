5f2267fb06000a281aae8629353a2eef8579efb6
Martin Weiss <u32martinweiss@proton.me>
2023-04-18 10:47:09 +0200

    FE: CSS Reset

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 
+import './styles/reset.scss';
+
 const App = (): JSX.Element => (
   <Routes>
     <Route path="/" element={<HomePage />} />
