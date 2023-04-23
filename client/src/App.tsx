244490bb34f4bfe850616278e9cda52af0e17cd0
Martin Weiss <u32martinweiss@proton.me>
2023-04-23 13:55:03 +0200

    FE: Protected routes system

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,47 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+
+import { useAuth } from './hooks/auth';
 
 import './styles/reset.scss';
 import './styles/main.scss';
 
-const App = (): JSX.Element => (
-  <Routes>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<RegisterPage />} />
-  </Routes>
-);
+interface IProtectedRouteProps {
+  condition: boolean;
+  fallbackPath: string;
+}
+
+const ProtectedRoute = ({
+  condition,
+  fallbackPath,
+}: IProtectedRouteProps): JSX.Element => {
+  return condition ? <Outlet /> : <Navigate to={fallbackPath} replace />;
+};
+
+const App = (): JSX.Element => {
+  const { auth } = useAuth();
+
+  if (typeof auth === 'undefined') {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route element={<ProtectedRoute condition={!auth} fallbackPath="/" />}>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+      <Route
+        element={<ProtectedRoute condition={!!auth} fallbackPath="/login" />}
+      >
+        <Route path="/logout" element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
+};
 
 export default App;
--- client/src/components/blocks/PageNavigation/PageNavigation.component.tsx
+++ client/src/components/blocks/PageNavigation/PageNavigation.component.tsx
@@ -1,20 +1,36 @@
 import { Link } from 'react-router-dom';
 
-const PageNavigation = (): JSX.Element => (
-  <nav>
-    <h2>Page Navigation</h2>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <li>
-        <Link to="/login">Sign in</Link>
-      </li>
-      <li>
-        <Link to="/register">Sign up</Link>
-      </li>
-    </ul>
-  </nav>
-);
+import { useAuth } from '../../../hooks/auth';
+
+const PageNavigation = (): JSX.Element => {
+  const { auth } = useAuth();
+
+  return (
+    <nav>
+      <h2>Page Navigation</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        {auth ? (
+          <>
+            <li>
+              <Link to="/logout"> Logout</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Sign in</Link>
+            </li>
+            <li>
+              <Link to="/register">Sign out</Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 export default PageNavigation;
