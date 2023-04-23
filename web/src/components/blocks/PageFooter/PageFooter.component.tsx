0abebc0d2463febf3c99dc7c82a314de6ae3f81a
Martin Weiss <u32martinweiss@proton.me>
2023-04-23 10:49:46 +0200

    WEB: Creating the Page Footer block

---
+++ web/src/components/blocks/PageFooter/PageFooter.component.tsx
@@ -0,0 +1,8 @@
+const PageFooter = (): JSX.Element => (
+  <footer>
+    <hr />
+    <p>Copyright &copy; 2023</p>
+  </footer>
+);
+
+export default PageFooter;
--- web/src/layouts/DefaultLayout.tsx
+++ web/src/layouts/DefaultLayout.tsx
@@ -1,4 +1,5 @@
 import PageNavigation from '../components/blocks/PageNavigation';
+import PageFooter from '../components/blocks/PageFooter';
 
 interface IProps {
   children: React.ReactNode;
@@ -8,6 +9,7 @@ const DefaultLayout = ({ children }: IProps): JSX.Element => (
   <>
     <PageNavigation />
     {children}
+    <PageFooter />
   </>
 );
 
